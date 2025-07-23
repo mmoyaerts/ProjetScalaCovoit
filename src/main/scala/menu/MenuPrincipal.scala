a6c6c05dcf9fbabbe4c8c875fbec60de05adb9fd
nathan.boileau <nathan.boileau@etu.uca.fr>
2025-07-23 10:45:18 +0200

    Réorganisation de la classe DBConnection : suppression de l'ancienne version et ajout d'une nouvelle implémentation avec gestion des connexions à la base de données.

--- src/main/scala/menu/MenuPrincipal.scala
+++
@@ -1,5 +0,0 @@
-package menu
-
-class MenuPrincipal {
-
-}
---
+++ src/main/scala/utils/DBConnection.scala
@@ -0,0 +1,25 @@
+import utils.DBConnection
+
+import java.sql.{Connection, DriverManager}
+import scala.util.{Failure, Success, Try}
+
+class DBConnection {
+  private val url = "jdbc:postgresql://localhost:5432/postgres"
+  private val username = "postgres"
+  private val password = "postgres"
+  
+  def getConnection: Try[Connection] = {
+    Try {
+      Class.forName("org.postgresql.Driver")
+      DriverManager.getConnection(url, username, password)
+    }
+  }
+  
+  def closeConnection(connection: Connection): Unit = {
+    Try(connection.close())
+  }
+}
+
+object DBConnection {
+  def apply(): DBConnection = new DBConnection()
+}
